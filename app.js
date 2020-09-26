9a9bb73f240731536a3894f9e3d1d3141eddc58b
Alek <alek.jakimovski1@gmail.com>
2020-09-26 14:07:34 +0200

    finished version

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const tableUsers = document.querySelector('.table-users');
 const addModalForm = document.querySelector('.add-modal .form');
 const modalWrapper = document.querySelector('.modal-wrapper');
 
+const editModal = document.querySelector('.edit-modal');
+const editModalForm = document.querySelector('.edit-modal .form');
+
+let id;
 
 //Create element and render the users
 const renderUser = (doc) => {
@@ -21,6 +25,20 @@ const renderUser = (doc) => {
     `;
     tableUsers.insertAdjacentHTML('beforeend', tr);
 
+    //edit member
+    const btnEdit = document.querySelector(`[data-id='${doc.id}'] .btn-edit`);
+    btnEdit.addEventListener('click', () => {
+        editModal.classList.add('modal-show');
+
+        id = doc.id;
+        editModalForm.firstName.value = doc.data().firstName;
+        editModalForm.lastName.value = doc.data().lastName;
+        editModalForm.email.value = doc.data().email;
+        editModalForm.phoneNumber.value = doc.data().phoneNumber;
+    });
+
+
+    //Delete member
     const btnDelete = document.querySelector(`[data-id='${doc.id}'] .btn-delete`);
     btnDelete.addEventListener('click', () => {
         db.collection('members').doc(`${doc.id}`).delete().then(() => {
@@ -32,6 +50,11 @@ const renderUser = (doc) => {
 //Click user add 
 addUser.addEventListener('click', () => {
     addModal.classList.add('modal-show');
+
+    addModalForm.firstName.value = '';
+    addModalForm.lastName.value = '';
+    addModalForm.phoneNumber.value = '';
+    addModalForm.email.value = '';
 })
 
 //Click outside modal
@@ -39,14 +62,24 @@ window.addEventListener('click', e => {
     if (e.target === addModal) {
         addModal.classList.remove('modal-show')
     }
-})
+    if (e.target === editModal) {
+        editModal.classList.remove('modal-show')
+    }
+});
 
-//Get All Users
-db.collection('members').get().then(querySnapshot => {
-    querySnapshot.forEach(doc => {
-        console.log(doc.data())
-        renderUser(doc);
-    })
+
+db.collection('members').onSnapshot(snapshot => {
+    snapshot.docChanges().forEach(change => {
+        if (change.type === 'added') {
+            renderUser(change.doc);
+        }
+        if (change.type === 'removed') {
+            let tr = document.querySelector(`[data-id='${change.doc.id}']`);
+
+            let tbody = tr.parentElement;
+            tableUsers.removeChild(tbody);
+        }
+    });
 })
 
 //Click submit in add modal
@@ -61,4 +94,15 @@ addModalForm.addEventListener('submit', (e) => {
     });
 
     modalWrapper.classList.remove('modal-show');
-})
+});
+
+editModalForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('members').doc(id).update({
+        firstName: editModalForm.firstName.value,
+        lastName: editModalForm.lastName.value,
+        email: editModalForm.email.value,
+        phoneNumber: editModalForm.phoneNumber.value
+    });
+    editModal.classList.remove('modal-show');
+});
